56f574a547167db10fea97cf58a01b7f0eba1d2b
takada291 <tkp291@gmail.com>
2026-02-03 13:55:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-report-byAI-v0.2.0'; // バージョンアップ
+const CACHE_NAME = 'daily-report-byAI-v0.2.1'; // バージョンアップ
 const urlsToCache = [
   './',
   './index.html',
@@ -49,3 +49,4 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
+
